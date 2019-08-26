028b580b40af557a75564971a063f45223eac475
Christina McMahon <chrisitina.mcmahon1@gmail.com>
2019-08-26 12:15:49 -0700

    Test LinkedList

--- src/tests/LinkedListClient.java
+++ src/tests/LinkedListClient.java
@@ -5,7 +5,18 @@ import lists.LinkedList;
 public class LinkedListClient {
 	public static void main(String[] args) {
 		LinkedList<String> list = new LinkedList<String>();
-		list.add("Bill");
+		list.add(0,"Augustus");
+		list.add(1,	"Balbinus");
+		list.add(2,	"Cresus");
+		list.add(3, "Commodus");
+		list.add(4, "Julius");
+		list.add(5, "Septimius");
+		list.add(6, "Tiberius");
+		list.add(7, "Valerian");
 		System.out.println(list);
+		System.out.println(list.getNode(7));
+		System.out.println(list.getPrevNode(0));
+		System.out.println(list.getNode(0));
+		System.out.println(list.indexOf("Valerian"));
 	}
 }
